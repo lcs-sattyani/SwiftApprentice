c826458ef9776c9f7b673633ed7dedd0371b4fd5
Sawsan Attyani <sattyani@lcs.on.ca>
2019-11-22 14:33:28 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Attyani, Sawsan on 2019-11-22.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
